9f1412a9526b50b1c8cb36b6a6ce9875b49ccb28
Benjamin Schmid <B.Schmid@exxcellent.de>
2012-03-01 14:32:24 +0100

    Minor package/module renames.

---
+++ src/main/java/de/exxcellent/beanvalidation/example/Customer.java
@@ -0,0 +1,78 @@
+/**
+ *  Copyright 2010 Gunnar Morling (http://www.gunnarmorling.de/)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package de.exxcellent.beanvalidation.example;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import de.exxcellent.beanvalidation.example.groups.CheckOnDeletion;
+import de.exxcellent.beanvalidation.example.groups.CheckOnUpdate;
+
+@Entity
+@NamedQueries({@NamedQuery(name = Customer.FIND_ALL_CUSTOMERS, query = "SELECT c FROM Customer c")})
+public class Customer {
+
+    public final static String FIND_ALL_CUSTOMERS = "findAllCustomers";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull(groups = CheckOnUpdate.class)
+    private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 80)
+    private String name;
+
+    @AssertTrue(groups = CheckOnDeletion.class)
+    private boolean archived;
+
+    public Customer() {
+
+    }
+
+    public Customer(String name) {
+        this.name = name;
+        archived = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isArchived() {
+        return archived;
+    }
+
+    public void setArchived(boolean archived) {
+        this.archived = archived;
+    }
+
+}
---
+++ src/test/java/de/exxcellent/beanvalidation/example/CustomerTest.java
@@ -0,0 +1,145 @@
+/**
+ *  Copyright 2010 Gunnar Morling (http://www.gunnarmorling.de/)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package de.exxcellent.beanvalidation.example;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.constraints.Size;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+
+    @SuppressWarnings("StaticNonFinalField")
+    private static EntityManagerFactory emf = null;
+
+    private EntityManager em;
+
+    @BeforeClass
+    public static void createEntityManagerFactory() {
+        emf = Persistence.createEntityManagerFactory("testPU");
+    }
+
+    @AfterClass
+    public static void closeEntityManagerFactory() {
+        if (emf != null) {
+            emf.close();
+        }
+    }
+
+    @Before
+    public void beginTransaction() {
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
+    }
+
+    @After
+    public void rollbackTransaction() {
+        if (em != null) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+
+            if (em.isOpen()) {
+                em.close();
+            }
+        }
+    }
+
+    @Test
+    public void nameTooShortOnInsert() {
+
+        try {
+            Customer customer = new Customer("Al");
+            em.persist(customer);
+            em.flush();
+            fail("Expected ConstraintViolationException wasn't thrown.");
+        } catch (ConstraintViolationException e) {
+            assertEquals(1, e.getConstraintViolations().size());
+            ConstraintViolation<?> violation = e.getConstraintViolations()
+                    .iterator().next();
+            assertEquals("name", violation.getPropertyPath().toString());
+            assertSame(Size.class, violation.getConstraintDescriptor()
+                    .getAnnotation().annotationType());
+        }
+    }
+
+    @Test
+    public void nameTooShortOnUpdate() {
+
+        try {
+            Customer customer = new Customer("Bob");
+            em.persist(customer);
+            em.flush();
+
+            em.detach(customer);
+            customer.setName("Bo");
+            em.merge(customer);
+            em.flush();
+
+            fail("Expected ConstraintViolationException wasn't thrown.");
+        } catch (ConstraintViolationException e) {
+            assertEquals(1, e.getConstraintViolations().size());
+            ConstraintViolation<?> violation = e.getConstraintViolations()
+                    .iterator().next();
+            assertEquals("name", violation.getPropertyPath().toString());
+            assertSame(Size.class, violation.getConstraintDescriptor()
+                    .getAnnotation().annotationType());
+        }
+    }
+
+    @Test
+    public void validCustomer() {
+
+        Customer customer = new Customer("Bob");
+        em.persist(customer);
+        em.flush();
+        Query query = em.createNamedQuery(Customer.FIND_ALL_CUSTOMERS);
+
+        assertEquals(1, query.getResultList().size());
+    }
+
+    @Test(expected = ConstraintViolationException.class)
+    public void nonArchivedCustomerDeleted() {
+
+        Customer customer = new Customer("Bob");
+        em.persist(customer);
+        em.remove(customer);
+        em.flush();
+    }
+
+    @Test
+    public void archivedCustomerDeleted() {
+
+        Customer customer = new Customer("Bob");
+        em.persist(customer);
+
+        customer.setArchived(true);
+        em.remove(customer);
+        em.flush();
+        assertTrue(em.createNamedQuery(Customer.FIND_ALL_CUSTOMERS)
+                           .getResultList().isEmpty());
+    }
+}
